8cb118456a4e60ebcd0b844ac61fde8889106ea3
atsb <atsb@atsb>
2019-11-28 23:17:34 +0100

    Fixed layout of code for scripts and change clipf.conf to clipfrc

--- scripts/gen_test_op.py
+++ scripts/gen_test_op.py
@@ -3,15 +3,19 @@
 import datetime
 from random import randint
 
+
 def run():
 
-    prods=['gr%02d.pr%02d' % (gr,pr) for gr in range(10) for pr in range(20)]
-    basedate=datetime.date(2000,01,01)
-    for day in range(365*5):
-        dt=basedate+datetime.timedelta(day)
-        dtiso=dt.isoformat()
-        print("%s:00:%s:%.2f:1:Note" % (dtiso,prods[0],20.0))
+    prods = ['gr%02d.pr%02d' % (gr, pr)
+             for gr in range(10) for pr in range(20)]
+    basedate = datetime.date(2000, 01, 01)
+    for day in range(365 * 5):
+        dt = basedate + datetime.timedelta(day)
+        dtiso = dt.isoformat()
+        print("%s:00:%s:%.2f:1:Note" % (dtiso, prods[0], 20.0))
         for i in range(20):
-            pr=prods[randint(10,199)]
-            print("%s:00:%s:%.2f:0:Note" % (dtiso,pr,1.0))
+            pr = prods[randint(10, 199)]
+            print("%s:00:%s:%.2f:0:Note" % (dtiso, pr, 1.0))
+
+
 run()
--- scripts/gen_test_prod.py
+++ scripts/gen_test_prod.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 
+
 def run():
 
     for gr in range(10):
         if gr:
-            dc='0'
+            dc = '0'
         else:
-            dc='1'
-        print("gr%02d.:0:Group %02d" % (gr,gr))
+            dc = '1'
+        print("gr%02d.:0:Group %02d" % (gr, gr))
         for pr in range(20):
-            print("gr%02d.pr%02d:%s:Product %02d%02d" % (gr,pr,dc,gr,pr))
+            print("gr%02d.pr%02d:%s:Product %02d%02d" % (gr, pr, dc, gr, pr))
+
+
 run()
